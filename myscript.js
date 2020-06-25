9951994b62ae2d4c5a719635b27bb2c8d3ed4510
Thabiso Mabote <mabote1@stolaf.edu>
2020-06-24 23:54:41 -0500

    Adding a fun times page

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-/*Home page portfolio user interface when hovering over images!*/
+/* Home page portfolio user interface when hovering over images!*/
 
 const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
 portfolioItems.forEach(portfolioItem => {
@@ -10,3 +10,39 @@ portfolioItems.forEach(portfolioItem => {
         portfolioItem.childNodes[1].classList.remove('img-darken');
     })
 })
+
+/* Slideshow user interface*/
+var slideIndex = 1;
+var timer = null;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("theSlides");
+  var descriptions = document.getElementsByClassName("theDescriptions");
+  var dots = document.getElementsByClassName("dot");
+  if (n==undefined){n = ++slideIndex}
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+      descriptions[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  descriptions[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  timer=setTimeout(showSlides, 3000);
+}
